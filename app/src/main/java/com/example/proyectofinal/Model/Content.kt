cd1158c7c783d92f44b7cac72afa3eaf1889b5e4
Iván Vázquez Martín <ivan.vazquez@live.u-tad.com>
2026-05-08 19:44:38 +0200

    BACKEND v0.4 -> Logica Gestion Examen y Actualizar alumnos (individual y en profesor) realizada. Rediseño de Data Class Content para siguiente versión de Backend (añadir contenido).

--- app/src/main/java/com/example/proyectofinal/Model/Content.kt
+++ app/src/main/java/com/example/proyectofinal/Model/Content.kt
@@ -2,9 +2,10 @@ package com.example.proyectofinal.Model
 
 data class Content(
     val id: String = "",
+    val contentType: String = "", // Entre: TECH, FORM, SET
     val number: Int = 0,
     val name: Map<String, String> = emptyMap(), // LocalizedString {es, en}
     val description: Map<String, String> = emptyMap(), // LocalizedString {es, en}
-    val url: String = "",
+    val url: String = "", // para video
     val beltId: String = "" // ID del cinturón al que pertenece (ej: "belt_yellow")
 )

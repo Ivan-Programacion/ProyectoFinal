9d2221855287be8a0b35dbcf3e8cd3d49b6e5db0
Iván Vázquez Martín <ivan.vazquez@live.u-tad.com>
2026-05-08 17:42:35 +0200

    Mejora en el servicio de notificaciones en primer plano

--- app/src/main/java/com/example/proyectofinal/Services/MyFirebaseMessagingService.kt
+++ app/src/main/java/com/example/proyectofinal/Services/MyFirebaseMessagingService.kt
@@ -3,10 +3,14 @@ package com.example.proyectofinal.Services
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.content.Context
+import android.content.Intent
+import android.app.PendingIntent
 import android.os.Build
 import androidx.core.app.NotificationCompat
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
+import com.example.proyectofinal.ViewModel.MainActivity
+import com.example.proyectofinal.R
 
 class MyFirebaseMessagingService : FirebaseMessagingService() {
 
@@ -31,14 +35,27 @@ class MyFirebaseMessagingService : FirebaseMessagingService() {
             val channel = NotificationChannel(channelId, "Notificaciones de la App", NotificationManager.IMPORTANCE_HIGH)
             notificationManager.createNotificationChannel(channel)
         }
+        // diseños visuales y funcionales de las notificaciones EN PRIMER PLANO
+        // Para que, cuando le pulses a la notificación, te abra la app
+        val intent = Intent(this, MainActivity::class.java).apply {
+            addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+        }
+        val pendingIntent = PendingIntent.getActivity(
+            this,
+            0,
+            intent,
+            PendingIntent.FLAG_ONE_SHOT or PendingIntent.FLAG_IMMUTABLE
+        )
 
         val notificationBuilder = NotificationCompat.Builder(this, channelId)
             .setContentTitle(title)
             .setContentText(body)
-            .setSmallIcon(android.R.drawable.ic_dialog_info) // Usa un ícono por defecto genérico de Android por ahora
+            .setSmallIcon(R.mipmap.logo_vector) // Para sacar el logo de la app
+            .setStyle(NotificationCompat.BigTextStyle().bigText(body)) // Para poder leer textos largos
+            .setPriority(NotificationCompat.PRIORITY_HIGH)
             .setAutoCancel(true)
+            .setContentIntent(pendingIntent)
 
         notificationManager.notify(System.currentTimeMillis().toInt(), notificationBuilder.build())
     }
 }
-
